ad4dc06bbf19c99f4d26213f50fbf6549e02af16
Mitzy <80993235+MitzyMo@users.noreply.github.com>
2024-04-14 16:12:55 -0500

    websocket and glitch testing

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { engine } from "express-handlebars";
+import {Server} from "socket.io"
 import productRouter from "./routes/productsRouter.js";
 import cartsRouter from "./routes/cartsRouter.js";
 import viewsRouter from "./routes/viewsRouter.js";
@@ -8,6 +9,7 @@ import __dirname from "./utils.js"; // Import __dirname from utils.js
 
 const PORT = 3000;
 const app = express();
+let serverSocket;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,10 +23,17 @@ app.use("/api/products", productRouter);
 app.use("/api/carts", cartsRouter);
 app.use("/", viewsRouter);
 
-app.listen(PORT, (error) => {
+const serverHTTP = app.listen(PORT, (error) => {
   if (error) {
     throw new Error("Failed to start the server:", error);
   }
   return console.log(`Server connected in port ${PORT}`);
 });
 
+// const io = new Server(serverHTTP); To call socket io.
+serverSocket = new Server(serverHTTP);
+
+//listens to any connection from the client
+serverSocket.on("connection", socket => {
+  console.log(`Client with id ${socket.id}`);
+}) //End of connection
--- src/routes/viewsRouter.js
+++ src/routes/viewsRouter.js
@@ -1,10 +1,27 @@
 import express from "express";
 const router = express.Router();
+import path from 'path'; // Import path module
+import __dirname from "../utils.js"; // Import __dirname from utils.js
+import ProductManagerModule from '../dao/ProductManager.js';
+const { ProductManager } = ProductManagerModule;
+let filePath = path.join(__dirname,'..','src', "data", "products.json");
+const manager = new ProductManager(filePath);
 
-router.get("/", (request, response) => {
-    response.setHeader("Content-Type", "text/html");
-    response.status(200).render('home',{});
-});
+router.get("/", async (request, response) => {
+    //console.log(filePath);
+    let {detail}= request.query
+    try {
+        let data = await manager.getProducts();
+        let limit = request.query.limit;
+        if (Number(limit) && limit > 0) {
+        data = data.slice(0, limit);
+        }
+        response.status(200).render('home',{data,detail,limit});
+    } catch (error) {
+        response.status(500).render('error',{ error: "Internal Server Error" });
+    }
+    });
+
 
 router.get("/realtimeproducts", (request, response) => {
     response.setHeader("Content-Type", "text/html");
