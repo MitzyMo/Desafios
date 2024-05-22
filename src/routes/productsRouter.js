eecceb497c54ca83d105ed479773260f01666d33
Mitzy <mitzy.moreno@masabi.com>
2024-05-22 00:01:49 -0500

    Adding Validatioin for product addition in cart

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -7,15 +7,15 @@ import {
   updateProduct,
   deleteProduct,
 } from "../controller/productController.js";
-import { auth } from "../middleware/auth.js";
+//import { auth } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/",getProducts);
 router.get("/paginate",getProductsPaginate);
 router.get("/:pid",getProductById);
-router.post("/", auth ,addProduct);
-router.put("/:pid", auth ,updateProduct);
-router.delete("/:pid", auth ,deleteProduct);
+router.post("/",addProduct);
+router.put("/:pid",updateProduct);
+router.delete("/:pid",deleteProduct);
 
 export default router;
