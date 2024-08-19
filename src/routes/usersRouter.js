1ebf6395f3d15b217ad9781154e1a2f1ce023ec9
Mitzy <mitzy.moreno@masabi.com>
2024-08-18 20:09:18 -0500

    Implementing multer to upload files.

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -5,19 +5,26 @@ import {
   updateUser,
   getUsers,
   getUserById,
+  uploadDocuments,
+  upgradeToPremium
 } from "../controller/userController.js";
 import { authRole } from "../middleware/authRole.js";
+import { upload } from "../utils/utils.js";
 
 export const router = Router();
 
 router.get("/",getUsers);
-
 router.get("/:uid", getUserById);
-
 router.post("/resetPassword", getResetPassword);
-
 router.post("/validateNewPassword/:token", getValidateNewPassword);
+router.post("/:uid/documents", upload.fields([
+  { name: 'profilePicture', maxCount: 1 },
+  { name: 'productImage', maxCount: 1 },
+  { name: 'identification', maxCount: 1 },
+  { name: 'proofOfAddress', maxCount: 1 },
+  { name: 'proofOfAccountStatus', maxCount: 1 }
+]), uploadDocuments);
+router.put("/premium/:uid", authRole(['admin']), upgradeToPremium);
 
-router.put("/premium/:uid",authRole(['admin']), updateUser);
 
 export default router;
